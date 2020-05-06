4af2b30607f1c329844714f2e3a02ed7a16df201
Zhao SHuyang <shuyang.zhao@etut.fi>
2020-05-06 22:22:26 +0300

    1 commit

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+		    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+
+(package-initialize)
+
+(load-theme 'zenburn  t)
+
+(custom-set-variables
+ '(inhibit-startup-screen t)
+ '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
+
+
+
