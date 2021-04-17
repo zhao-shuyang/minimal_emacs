0887e8011da114b46c98ae74b215306b1ab5ef03
zhao-shuyang <m@zsy.fi>
2021-04-17 19:35:44 +0000

    beautify the lines

--- init.el
+++ init.el
@@ -12,11 +12,15 @@
 (load-theme 'zenburn  t)
 
 (custom-set-variables
+ '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))) 
  '(inhibit-startup-screen t)
- '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))))
+ '(ivy-mode t)
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil)
+ )
 
+;; Using ibuffer 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
 (add-hook 'ibuffer-mode-hook (lambda () (ibuffer-auto-mode 1)))
-(ivy-mode 1)
-(tool-bar-mode -1)
+
+
