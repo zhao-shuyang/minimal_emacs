defd8ac3c15a5aa2f53ef13882d243539107d454
lord.ouoh@gmail.com <shuyang.zhao@etut.fi>
2021-09-13 22:34:19 +0300

    having two init.el for gui and terminal

---
+++ gui/init.el
@@ -0,0 +1,141 @@
+(setq user-full-name "Zhao Shuyang")
+(setq user-mail-address "contact@zhaoshuyang.com")
+
+;; Initialize pakcage ecosystem
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+;; This a emacs configuration file following emacs from scratch
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux and Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; GUI setup
+(setq inhibit-startup-screen t)
+;; (setq visible-bell t)
+
+(setq frame-title-format "%b")
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(column-number-mode)
+
+(global-auto-complete-mode t)
+;; (blink-cursor-mode 1)
+
+;;(size-indication-mode t)
+;;(global-display-line-numbers-mode t)
+(set-face-attribute 'default nil :font "M+ 2m" :height 190)
+(set-face-attribute 'fixed-pitch nil :font "M+ 1mn" :height 190)
+(set-face-attribute 'variable-pitch nil :font "M+ 2m" :height 190)
+
+
+;; Key bindings
+(require 'iso-transl)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-o") 'other-window)
+
+;; Indent setup
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tabs-mode t)
+
+;;(use-package doom-themes
+  ;;:init (load-theme 'doom-palenight t))
+
+(use-package hc-zenburn-theme
+  :ensure t
+  :load-path "themes"
+  :init
+  (setq hc-zenburn-theme t)
+  :config
+  (load-theme 'hc-zenburn t))
+
+(use-package all-the-icons :defer 0.5)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 14)))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+		 :map ivy-minibuffer-map
+         ("RET" . ivy-alt-done)
+		 )
+  :init (ivy-mode 1))
+
+(use-package counsel
+  :diminish
+  :config
+  (counsel-mode 1))
+
+(use-package which-key
+  :diminish
+  :defer 0
+  :config (which-key-mode)
+  (setq which-key-idle-delay 1))
+
+(use-package flycheck
+  :init (global-flycheck-mode t)
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
+)
+
+(use-package magit
+  :commands magit-status
+)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Project")
+    (setq projectile-project-search-path '("~/Project")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+
+(use-package org)
+
+;;(use-package jedi)
+;;(add-hook 'python-mode-hook 'jedi:setup)
+;;(setq jedi:complete-on-dot t)   
+
+
+
+
+;; Elpy requires flake8
+;;(setq elpy-rpc-python-command "python3")
+;;(use-package elpy
+  ;;:init (elpy-enable)
+  ;;:custom (elpy-rpc-python-command "python3"))
+;;(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
+
+;; For C++
+
+(use-package tex
+  :defer t
+  :ensure auctex
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-PDF-mode t))
+
+
+
+;;; init.el ends here
+
+
